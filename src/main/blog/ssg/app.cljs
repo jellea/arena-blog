aa077011329703dc20a70a8daed5730e7fca4462
Jelle Akkerman <jellea@users.noreply.github.com>
2019-11-29 23:37:04 +0100

    markdown and css added

--- src/main/blog/ssg/app.cljs
+++ src/main/blog/ssg/app.cljs
@@ -2,7 +2,8 @@
 
 (def fetch (js/require "node-fetch"))
 (def hb (js/require "handlebars"))
-  (def fs (js/require "fs"))
+(def fs (js/require "fs"))
+(def md (js/require "marked"))
 
 (def token js/process.env.ARENA_ACCESS_TOKEN)
 
@@ -10,21 +11,23 @@
   (prn x)
   x)
 
-(defn hello []
-    (-> (fetch "https://api.are.na/v2/channel/words-e6vp8lael4m/feed" #js {"headers" #js {"access_token" token}})
-      (.then (fn [res] (.json res)))
-      (.then (fn [json] (js->clj json :keywordize-keys true)))
-      (.then (fn [data] (->> data 
-                             :items
-                             (filter #(= "Text" (-> % :item :class)))
-                             (map #(-> % :item (select-keys [:generated_title :content])))
-                             log)))
-      (.then (fn [items] ((hb.compile (fs.readFileSync "./views/index.html" #js {"encoding" "utf8"})) (clj->js {:items items}))))
-      (.then (fn [html] (prn [:html html])
-        (fs.writeFileSync "./.data/static/index.html" html)))
-      ;;(.catch (fn [] (js/process.exit 1)))
-  (.then (fn [] (js/process.exit)))))
+;;(log (md "#hello"))
+
+(defn gen-site []
+  (-> (fetch "https://api.are.na/v2/channel/words-e6vp8lael4m/feed" #js {"headers" #js {"access_token" token}})
+    (.then (fn [res] (.json res)))
+    (.then (fn [json] (js->clj json :keywordize-keys true)))
+    (.then (fn [data] (->> data 
+                           :items
+                           (filter #(= "Text" (-> % :item :class)))
+                           (map #(-> % :item (select-keys [:generated_title :content])))
+                           (map #(update % :content md))
+                           log)))
+    (.then (fn [items] ((hb.compile (fs.readFileSync "./views/index.html" #js {"encoding" "utf8"})) (clj->js {:items items}))))
+    (.then (fn [html] (prn [:html html])
+      (fs.writeFileSync "./.data/static/index.html" html)))
+    ;;(.catch (fn [] (js/process.exit 1)))
+    (.then (fn [] (js/process.exit)))))
         
-  (defn main [& cli-args]
-  (println "Hello World2")
-  (hello))
+(defn main [& cli-args]
+  (gen-site))
